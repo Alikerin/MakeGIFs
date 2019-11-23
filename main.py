c0162f658264d10aa312d7657b3ad446da5e1ab3
Ibrahim Salihu Yusuf <sibrahim1396@gmail.com>
2019-11-23 16:20:07 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
-from utils import MakeGIF
+from utils import *
 
 def main():
     import argparse
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--image_dir', '-imgdir', type=str, required=True)
+    parser.add_argument('--image_dir', '-imgdir', type=str)
     parser.add_argument('--output_file_name', '-fn', type=str, required=True)
     parser.add_argument('--duration_of_frame', '-dur', type=int, default=60)
     parser.add_argument('--no_of_loops', '-lps', type=int, default=3)
-
+    parser.add_argument('--event_file', '-evf', type=str)
+    parser.add_argument('--video_name', '-vn', type=str)
+    parser.add_argument('--type', choices=['1', '2'], help='1 for image_dir and 2 for event_file')
     args = parser.parse_args()
 
     #convert args to dictionary
     params = vars(args)
+    if params['type'] == '1':
+        assert params['image_dir'] != None, 'Option 1 requires image_dir argument'
+        gif_object = MakeGIF(params['image_dir'])
+        gif_object.makegif(params['output_file_name'], params['duration_of_frame'], params['no_of_loops'])
 
-    gif_object = MakeGIF(params['image_dir'])
-
-    gif_object.makegif(params['output_file_name'], params['duration_of_frame'], params['no_of_loops'])
+    if params['type'] == '2':
+        assert params['event_file'] != None, 'Option 1 requires image_dir argument'
+        gif_object = TensorboardMakeGIF(params['event_file'], params['video_name'])
+        gif_object.makegif(params['output_file_name'], params['duration_of_frame'], params['no_of_loops'])
 
 if __name__ == '__main__':
     main()
--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ Number the pictures in an ascending order e.g 1, 2, 3, 4 etc.
 '''
 import os
 from PIL import Image
+import tensorflow as tf
 
 class MakeGIF:
     def __init__(self, image_dir):
@@ -21,3 +22,28 @@ class MakeGIF:
         for name in self.image_names:
             self.images.append(Image.open(os.path.join(self.image_dir, name)))
         self.images[0].save(output_file_name, save_all=True, append_images=self.images[1:], optimize=False, duration=duration_of_frame, loop=no_of_loops)
+
+class TensorboardMakeGIF:
+    def __init__(self, event_file, video_name):
+        self.event_file = event_file
+        self.video_name = video_name
+
+        self.events = [value.tag for event in tf.compat.v1.train.summary_iterator(self.event_file) for value in event.summary.value]
+
+        assert self.video_name in self.events, '{} not in Event file'.format(self.video_name)
+
+
+    def makegif(self, output_file_name, duration_of_frame, no_of_loops):
+        assert (output_file_name[-4:] == '.gif'), 'Expects file extension to be .gif but got {}'.format(output_file_name)
+        self.x = 0
+        for event in tf.compat.v1.train.summary_iterator(self.event_file):
+            for value in event.summary.value:
+                if value.tag == self.video_name:
+                    self.x = value
+        self.image_string = self.x.image.encoded_image_string
+        self.image = tf.image.decode_gif(self.image_string)
+        self.image = self.image.numpy()
+
+        self.image_list = [Image.fromarray(self.image[i]) for i in range(self.image.shape[0])]
+
+        self.image_list[0].save(output_file_name, save_all=True, append_images=self.image_list[1:], optimize=False, duration=duration_of_frame, loop=no_of_loops)
